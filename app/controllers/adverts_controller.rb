97e98b63edc1f3e23dcd375ed6be32f82e2dba7b
el-fuego <watt87@mail.ru>
2013-11-24 15:04:51 +0200

    offers search rendering

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,7 +1,16 @@
 class AdvertsController < ApplicationController
   def index
     @adverts = []
-    offers = BestOffer.order('price_difference DESC').limit(100)
+
+    offers = BestOffer
+    if params['model_id']
+      offers = offers.model params['model_id']
+    else
+      offers = offers.model ManufacturerModel.where({manufacturer_id: params['manufacturer_id']}).map{|m|m.id} if params['manufacturer_id']
+    end
+    offers = offers.year params['year']  if params['year']
+
+    offers = offers.limit(100)
     @adverts = offers.map{|o| o.advert} if !offers.empty?
   end
 end
--- app/models/best_offer.rb
+++ app/models/best_offer.rb
@@ -1,3 +1,11 @@
 class BestOffer < ActiveRecord::Base
   belongs_to :advert
+
+  scope :model, lambda{ |id|
+    where(manufacturer_model_id: id) if id
+  }
+
+  scope :year, lambda{ |year|
+    where(manufacture_year: year) if year
+  }
 end
